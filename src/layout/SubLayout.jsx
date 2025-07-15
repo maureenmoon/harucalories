05f8c95b2f28c83bbe39999b5bde5796aa47f94e
oseonz <seojjin1220@naver.com>
2025-07-15 17:55:34 +0900

    푸터, 서브레이아웃 수정

--- src/layout/SubLayout.jsx
+++ src/layout/SubLayout.jsx
@@ -4,20 +4,17 @@ import { Link } from "react-router-dom";
 function SubLayout({ to, menu, label }) {
   return (
     <div className="w-full bg-white border-b border-gray-300 flex justify-center items-center p-3 pb-0">
-      <div className="container w-[1020px] pt-4 md:pt-8 pb-4 flex flex-col items-start text-gray-600 md:flex-row md:items-start mt-4">
-        <Link to={to}>
-          <div className="flex items-center justify-center">
-            <p className="flex items-center text-base md:text-xl text-gray-400 font-medium hover:underline cursor-pointer">
-              <span>{menu} </span>
-              <span className="hidden md:inline-block ml-1 text-xl align-middle">
-                ＞
-              </span>
-            </p>
-          </div>
-        </Link>
-        <h1 className="text-3xl md:text-xl font-semibold text-center md:text-left mt-0 md:mt-0">
-          {label}
-        </h1>
+      <div className="container w-[1020px] pt-2 md:pt-2 pb-1 flex flex-col items-start text-gray-600 md:flex-row md:items-start mt-4">
+        <div className="breadcrumbs text-lg">
+          <ul>
+            <li>
+              <Link to={to}>
+                <a className="text-gray-400">{menu}</a>
+              </Link>
+            </li>
+            <li className="font-semibold">{label}</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
